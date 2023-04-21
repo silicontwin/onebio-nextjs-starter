b012519e5647b489d821d31eef524d3d33f2baab
Nick Warren <github@oneb.io>
2023-04-20 20:46:53 -0400

    added a link to the basic example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="p-20">
+      <h1 className="font-bold text-lg mb-4">
+        <a href="https://oneb.io" target="_blank">
+          OneBio
+        </a>{' '}
+        Next.js Examples
+      </h1>
+
+      <ul>
+        <li>
+          <Link href="/basic" className="text-blue-600 underline">
+            Basic
+          </Link>
+          : displays a member's thought stream with no frills
+        </li>
+      </ul>
+    </div>
+  );
 }
